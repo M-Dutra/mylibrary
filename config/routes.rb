3430cd19d0ccd2221f8e66e9151e3c6639fb7454
Manuel Dutra <manuel.f.dutra8@gmail.com>
2023-05-17 11:31:00 +0200

    Delete pages controller(it was just a test), routes,controller,views - BOOKS - INDEX

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
-  get 'about', to: "pages#about"
+  root 'books#index'
+  resources :books, only: [:index]
+
 end
