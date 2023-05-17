66b7c1126958116e86f7060ef160520320eedbcd
Manuel Dutra <manuel.f.dutra8@gmail.com>
2023-05-17 12:21:44 +0200

    Controller books - action - new create

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,22 @@
 class BooksController < ApplicationController
-  def index
+  def new
+    @book = Book.new
   end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to @book
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :author, :year, :photo)
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   root 'books#index'
-  resources :books, only: [:index]
+  resources :books, only: [:new, :create]
 
 end
