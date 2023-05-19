a1d4dafcd442d8542b3ad9cf0b85024592d98caf
Manuel Dutra <manuel.f.dutra8@gmail.com>
2023-05-19 22:46:23 +0200

    flash alert to verify if destroy action really happen and before_action require_user

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,9 @@
 class BooksController < ApplicationController
   before_action :authenticate_user!, only: %i[new create]
+  before_action :require_user, only: %i[index new create destroy]
 
   def index
-    @books = current_user.books
+    @books = current_user.books.all 
   end
 
   def new
@@ -10,21 +11,25 @@ class BooksController < ApplicationController
   end
 
   def create
-    @book = Book.new(book_params)
+    @book = current_user.books.build(book_params)
     @book.user = current_user
     if @book.save
       flash[:notice] = "Book was created successfully"
       redirect_to books_path
     else
-      render 'new'
+      render 'new', status: :unprocessable_entity
     end
   end
 
   def destroy
     @book = Book.find(params[:id])
     @book.destroy
-    flash[:alert] = "Book was destroyed successfully"
     redirect_to books_path
+    if @book.destroyed?
+      flash[:alert] = "Book was destroyed successfully"
+    else
+      flash[:alert] = "Failed to destroy the book"
+    end
   end
 
   private
@@ -33,4 +38,10 @@ class BooksController < ApplicationController
     params.require(:book).permit(:title, :author, :year, :photo, :rating)
   end
 
+  def require_user
+    if !user_signed_in?
+      flash[:alert] = "You must be logged in to perform that action"
+      redirect_to new_user_session_path
+    end
+  end
 end
